6551a8ed6687e8ca7ef372aef9f0e35b1b17076c
afriansyah <alfinafriansyah68@gmail.com>
2024-05-27 11:22:21 +0700

    commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="static/css/style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    device-width, initial-scale=1.0">
+    <title>ANIMAZONE</title>
+    
+</head>
+<body>
+    <div class="container">
+        <header>
+            
+            <div class="logo">
+                <i class="fas fa-gamepad"></i>
+                <p>Zona Pengetahuan Fauna</p>
+            </div>
+            
+            <ul class="navbar">
+                <li><a href="#topup">KATEGORI</a></li>
+                <li><a href="#kita">CONTACT US</a></li>
+            </ul>
+        </header>
+        <div class="content-wrapper">
+            <form action="/login" method="post">
+            <div class="content-desc">
+                
+                <h1>Ensiklopedia Hewan</h1>
+                <p>Merupakan website yang berguna untuk lebih mengenal tentang hewan 
+                </p>
+                    
+                <a href="#topup" id="btn2" style="text-decoration: none;color:white" type="submit">START</a>
+            </div>
+            </form>
+            <div class="banner">
+                <img src = "static/images/banner.png">
+            </div>
+        </div>
+    </div>
+
+    <div class="logo-section" id="topup">
+        <i class="fas fa-gamepad"></i>
+        <h1>KATEGORI</h1>
+    </div>
+    
+    <section class="section-1">
+        <div class="section-1-wrapper">  
+            <h1></h1>
+            <br>
+            <div class="section-1-img">
+                <a href="karnivora.php"><div class="img img-1"></div></a>
+                <a href="herbivora.php"><div class="img img-2"></div></a>
+                <a href="omnivora.php"><div class="img img-3"></div></a>
+            </div>
+        </div>
+        
+
+    </section>
+
+    <footer>
+        <div class="footer-body">
+            <div class="logo footer-logo" id="kita">
+                <i class="fas fa-gamepad"></i>
+                <h1>About Us </h1>
+            </div>
+            <ul>
+                <h3>Ega Yudhistira Y.P </h3>
+                <li><a href="https://www.instagram.com/p/CJqFfZXlm5x/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="><i class="fab fa-instagram"></i>Instagram</a></li>
+            </ul>
+            <ul>
+                <h3>Dwi Nur Widayanti</h3>
+                <li><a href="https://www.instagram.com/dw.ty_?igsh=MWJ1MzVmMTNxZ25qNg==x"><i class="fab fa-instagram"></i>Instagram</a></li>
+            </ul>
+            <ul>
+                <h3>Hestika Fitriani</h3>
+                <li><a href="https://www.instagram.com/hestika_16?igsh=MXV3dnZkYXQ4cXNkZA=="><i class="fab fa-instagram"></i>Instagram</a></li>
+            </ul>
+        </div>
+    </footer>
+    <div class="footer-bottom">
+        <p>SEMANGAT &copy; semoga WEB nya membantu | Designed <span>By KELOMPOK 11 </span> 2024</p>
+    </div>
+</body>
+</html>
---
+++ omnivora.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+    <link rel="stylesheet" href="static/css/omnivora.css">
+    <link rel="stylesheet" href="static/css/ayam.css">
+    <link rel="stylesheet" href="static/css/babi.css">
+    <link rel="stylesheet" href="static/css/tikus.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    device-width, initial-scale=1.0">
+    <title>ANIMAZONE</title>
+    
+</head>
+
+<body>
+<div class="container">
+        <header>
+            
+            <div class="logo">
+                <i class="fas fa-gamepad"></i>
+                <p>Zona Pengetahuan Fauna</p>
+            </div>
+            
+            <ul class="navbar">
+                <li><a href="#hewan" class="active">HEWAN</a></li>
+                <li><a href="index.php" class="active">KEMBALI</a></li>
+            </ul>
+        </header>
+        <div class="content-wrapper">
+            <form action="/login" method="post">
+            <div class="content-desc">
+                
+                <h1>OMNIVORA</h1>
+                <p>Hewan omnivora adalah hewan-hewan pemakan daging dan tumbuhan,
+alias pemakan segalanya. Manusia termasuk omnivora sejati. Manusia memakan
+daging-dagingan dan juga tumbuh-tumbuhan sebagai sayuran. Selain manusia,
+monyet, tikus, babi, luwak, dan bekantan juga termasuk contoh hewan omnivora.
+Ciri-ciri dari hewan dalam golongan ini adalah struktur giginya yang terdiri atas
+gigi seri, gigi taring, dan gigi geraham secara lengkap. Gigi seri berbentuk pipih
+digunakannya untuk memotong makanan, gigi taring digunakan untuk mencabik
+daging, sedangkan gigi geraham digunakan untuk mengunyah.
+                </p>
+                    
+            </div>
+            </form>
+            <div class="banner">
+                <img src = "static/images/logoom.png">
+            </div>
+        </div>
+    </div>
+    <div class="logo-section" id="hewan">
+        <i class="fas fa-gamepad"></i>
+        <h1>HEWAN</h1>
+    </div>
+    
+    <section class="section-1">
+        <div class="section-1-wrapper">  
+            <h1></h1>
+            <br>
+            <div class="section-1-img">
+                <a href="#ayam"><div class="img img-1"></div></a>
+                <a href="#babi"><div class="img img-2"></div></a>
+                <a href="#tikus"><div class="img img-3"></div></a>
+            </div>
+        </div>
+    </section>
+    
+    <div class="ayam" id="ayam">
+    <div class="ayam__content">
+        <div class="ayam__img">
+            <img src="static/images/ayam1.jpeg" alt="rubah">
+            <img src="static/images/ayam2.jpeg" alt="ular">
+            <img src="static/images/ayam3.jpeg" alt="macan">
+            <a href="#hewan" class="ayam__close">&times;</a>
+        </div>
+        <div class="ayam__header">
+            <h1>Kukuruyuk Yang Rakus</h1>
+        </div>
+        <div class="ayam__text">
+        <p>Ayam omnivora adalah jenis ayam yang memiliki kecenderungan untuk memakan berbagai jenis makanan, baik tumbuhan maupun hewan. Dalam pola makanannya, ayam omnivora tidak terlalu membatasi diri pada satu jenis sumber nutrisi, melainkan dapat mengonsumsi biji-bijian, serangga, cacing, buah-buahan, dan dedaunan. Kemampuan adaptasi ini memungkinkan mereka untuk mendapatkan nutrisi yang diperlukan dari berbagai sumber makanan yang tersedia di lingkungan mereka, menjadikannya salah satu jenis ayam yang cukup tangguh dan mudah beradaptasi di berbagai habitat. </p>
+        <p></p>
+    </div>
+    <a href="#hewan" class="btn ayam__btn"></a>
+    </div>
+</div>
+
+<div class="babi" id="babi">
+    <div class="babi__content">
+        <div class="babi__img">
+            <img src="static/images/babi1.jpeg" alt="babi">
+            <img src="static/images/babi2.jpeg" alt="ular">
+            <img src="static/images/babi3.jpeg" alt="macan">
+            <a href="#hewan" class="babi__close">&times;</a>
+        </div>
+        <div class="babi__header">
+            <h1>Imut Tapi Rakus</h1>
+        </div>
+        <div class="babi__text">
+        <p>Babi omnivora adalah hewan yang memiliki kebiasaan makan beragam jenis makanan, baik dari tumbuhan maupun hewan. Dalam pola makannya, babi tidak memilih-milih, mereka dapat mengonsumsi akar, rumput, buah-buahan, serangga, serta kadang-kadang memakan bangkai hewan. Kemampuan ini membuat mereka menjadi pemakan segala, sehingga secara alami mereka memainkan peran penting dalam mengatur ekosistem dengan membersihkan sisa-sisa organik. Babi omnivora juga sering dipelihara untuk dagingnya yang banyak dikonsumsi oleh manusia di berbagai belahan dunia. </p>
+        <p></p>
+    </div>
+    <a href="#hewan" class="btn babi__btn"></a>
+    </div>
+</div>
+
+<div class="tikus" id="tikus">
+    <div class="tikus__content">
+        <div class="tikus__img">
+            <img src="static/images/tikus1.jpeg" alt="tikus">
+            <img src="static/images/tikus2.jpeg" alt="ular">
+            <img src="static/images/tikus3.jpeg" alt="macan">
+            <a href="#hewan" class="tikus__close">&times;</a>
+        </div>
+        <div class="tikus__header">
+            <h1>Sukanya Di Tempat Kotor</h1>
+        </div>
+        <div class="tikus__text">
+        <p>
+Tikus adalah hewan omnivora yang memiliki kebiasaan makan yang bervariasi, mereka mampu mengonsumsi berbagai jenis makanan, baik yang berasal dari tumbuhan maupun hewan. Dalam habitatnya, tikus dapat memakan biji-bijian, buah-buahan, akar, serangga, dan kadang-kadang bahkan daging. Kehadiran mereka dalam ekosistem dapat mempengaruhi siklus nutrisi dan populasi organisme lainnya. Keadaptabilitas mereka terhadap berbagai sumber makanan membuat tikus menjadi hewan yang sukses menyesuaikan diri dengan berbagai lingkungan, sehingga sering dianggap sebagai hama yang merugikan dalam pertanian dan kesehatan manusia. </p>
+        <p></p>
+    </div>
+    <a href="#hewan" class="btn tikus__btn"></a>
+    </div>
+</div>
+
+</body>
